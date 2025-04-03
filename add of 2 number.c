76d3a9b3933b0aeab1627f5e6f3bcf50f8f66004
Ayush8512 <ayushpandey111223@gmail.com>
2025-04-03 10:40:04 +0530

    code of addition of 2 numbers

---
+++ add of 2 number.c
@@ -0,0 +1,9 @@
+# include<stdio.h>
+int main(){
+    int a,b,sum=0;
+    printf("value of a and b are respectively");
+    scanf("%d"&a,b);
+    printf("a is %d \n b is %d ",a,b);
+    sum=a+b;
+    printf("sum of %d and %d is %d",a,b,sum);
+}
